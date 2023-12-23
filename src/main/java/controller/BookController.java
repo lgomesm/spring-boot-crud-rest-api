4b41a199d6a7d10d5f1ffc2129305b73b6280c3f
Lucas <lucasgomesmorais58@gmail.com>
2023-12-23 10:52:43 -0300

    Last settings for the project

--- src/main/java/controller/BookController.java
+++ src/main/java/controller/BookController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import entity.Book;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,54 +10,84 @@ import org.springframework.web.bind.annotation.*;
 import repository.BookRepository;
 
 import java.util.List;
+import java.util.Optional;
 
+@RestController
 public class BookController {
+
+    Logger logger = LoggerFactory.getLogger(BookController.class);
 
     @Autowired
     public BookRepository bookRepository;
 
     @GetMapping("/books")
-    public List<Book> getAllBooks() {
-        return bookRepository.findAll();
+    public ResponseEntity<List<Book>> getAllBooks() {
+        try {
+            List<Book> books = bookRepository.findAll();
+            return new ResponseEntity<>(books, HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error("Error getting all books", e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("/books/{id}")
     public ResponseEntity<Book> getBook(@PathVariable Long id) {
-        Book book = bookRepository.findById(id).orElse(null);
-
-        if (book != null) {
-            return ResponseEntity.ok(book);
-        } else {
-            return ResponseEntity.notFound().build();
+        try {
+            Book book = bookRepository.findById(id).orElseThrow(() -> new BookNotFoundException("Book not found with id " + id));
+            return new ResponseEntity<>(book, HttpStatus.OK);
+        } catch (BookNotFoundException e) {
+            logger.error("Book not found with id " + id, e);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            logger.error("Error getting book with id " + id, e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @PostMapping("/book/add")
     public ResponseEntity<Book> createBook(@RequestBody Book book) {
-        Book newBook = bookRepository.save(book);
-        return ResponseEntity.status(HttpStatus.CREATED).body(newBook);
+        try {
+            Book newBook = bookRepository.save(book);
+            return new ResponseEntity<>(newBook, HttpStatus.CREATED);
+        } catch (Exception e) {
+            logger.error("Error creating book", e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PutMapping("/book/update/{id}")
     public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book updatedBook) {
-        Book book = bookRepository.findById(id).orElse(null);
-
-        if (book != null) {
+        try {
+            Book book = bookRepository.findById(id).orElseThrow(() -> new BookNotFoundException("Book not found with id " + id));
             book.setTitle(updatedBook.getTitle());
             book.setAuthor(updatedBook.getAuthor());
             book.setIsbn(updatedBook.getIsbn());
-
             bookRepository.save(book);
-
-            return ResponseEntity.ok(book);
-        } else {
-            return ResponseEntity.notFound().build();
+            return new ResponseEntity<>(book, HttpStatus.OK);
+        } catch (BookNotFoundException e) {
+            logger.error("Book not found with id " + id, e);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            logger.error("Error updating book with id " + id, e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @DeleteMapping("/book/delete/{id}")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
-    public void removeBook(@PathVariable Long id) {
-        bookRepository.findById(id).ifPresent(book -> bookRepository.delete(book));
+    public ResponseEntity<Void> removeBook(@PathVariable Long id) {
+        try {
+            Optional<Book> bookOptional = bookRepository.findById(id);
+            if (bookOptional.isPresent()) {
+                bookRepository.delete(bookOptional.get());
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            logger.error("Error deleting book with id " + id, e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
